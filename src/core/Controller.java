1fe4d41c2c267f667e3fae53ae8bbbd50dfae6fe
Christopher McMorran <chris.mcmorran@carleton.ca>
2016-12-09 02:21:55 -0500

    Error handling and commenting. Also moved the exit button.
    Signed-off-by: Christopher McMorran <chris.mcmorran@carleton.ca>

--- src/core/Controller.java
+++ src/core/Controller.java
@@ -5,6 +5,7 @@ import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -62,16 +63,62 @@ public class Controller implements Initializable {
     private void handleNumberOfNodesChanged() {
         String text = this.nodeField.getText();
         if (IO.isInteger(text) && Integer.valueOf(text) > 0) {
-            System.out.println("Valid integer. Changing number of nodes.");
-            numberOfNodes = Integer.valueOf(text);
-            System.out.println("Number of nodes is now: " + numberOfNodes);
-            System.out.println("Redrawing nodes...");
-            redrawNodes();
-        } else {
+            handleValidNodeChange(text);
+        } else if (IO.isInteger(text) && Integer.valueOf(text) == 0) {
+            handleZeroNodes();
+        } else if (text.length() == 0) {
+            // The user is probably entering a new number.
             System.err.println("Invalid input. Waiting for a positive integer value.");
+        } else {
+            // The user is probably inputting characters or negative values.
+            handleInvalidNumberOfSesnors();
         }
     }
 
+    /**
+     * Handles the event when a valid number is entered for the number of nodes.
+     *
+     * @param text The text that was entered into the number of nodes.
+     */
+    private void handleValidNodeChange(String text) {
+        System.out.println("Valid integer. Changing number of nodes.");
+        numberOfNodes = Integer.valueOf(text);
+        System.out.println("Number of nodes is now: " + numberOfNodes);
+        System.out.println("Redrawing nodes...");
+        redrawNodes();
+    }
+
+    /**
+     * Handles the event when 0 is entered.
+     */
+    private void handleZeroNodes() {
+        System.out.println("Value is 0. Removing nodes...");
+        numberOfNodes = 0;
+        redrawNodes();
+        radiusField.setText("No Sensors.");
+    }
+
+
+    /**
+     * Displays an alert message, sets the number of nodes to 0 and sets the radius input field
+     * to an error message.
+     */
+    private void handleInvalidNumberOfSesnors() {
+        // Remove the nodes due to invalid input.
+        numberOfNodes = 0;
+        this.nodeField.setText("0");
+        redrawNodes();
+        radiusField.setText("Invalid Input.");
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Invalid Input");
+        alert.setHeaderText("Invalid Number of Sensors!");
+        alert.setContentText("Please ensure that you have entered a valid number of sensors.");
+        alert.showAndWait();
+    }
+
+    /**
+     * Called to change the value of the sensor radix.
+     */
     private void handleRadiusChange() {
         String text = this.radiusField.getText();
         if (IO.isInteger(text) && Integer.valueOf(text) > 0) {
@@ -86,9 +133,11 @@ public class Controller implements Initializable {
     }
 
 
+    /**
+     * Redraws all of the nodes on the foreground.
+     */
     private void redrawNodes() {
         nodeRadius = windowWidth / numberOfNodes;
-
         windowPane.getChildren().clear();
         windowPane.getChildren().addAll(originalWindow);
         redrawSpriteLoop();
@@ -96,9 +145,12 @@ public class Controller implements Initializable {
 
     }
 
+    /**
+     * Redraws the radius of the nodes.
+     * Calls the sprite update method.
+     */
     private void redrawRadius() {
         numberOfNodes = (int) (windowWidth / nodeRadius) + 1;
-
         windowPane.getChildren().clear();
         windowPane.getChildren().addAll(originalWindow);
         redrawSpriteLoop();
@@ -106,6 +158,9 @@ public class Controller implements Initializable {
 
     }
 
+    /**
+     * Causes the sprites to be called and move around "performing the algorithm".
+     */
     private void redrawSpriteLoop() {
         TranslateTransition transition;
         double x;
